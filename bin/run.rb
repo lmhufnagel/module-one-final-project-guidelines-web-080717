07b22da7ff29178c65a752d105eb5d37adcd592e
lmhufnagel <lmhufnagel@gmail.com>
2017-08-21 16:05:12 -0400

    cli

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,11 @@
 require_relative 'config/environment'
 
+
+def welcome
+  puts "Hey there! #{username}! We want to hear what you have to say!"
+end
+
+def get_character_from_user
+  puts "What do you think will make our project better? Please drop a note in our suggestion box!"
+  gets.chomp
+end
