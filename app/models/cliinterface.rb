c0937d2c4f75b2db22a92788e9ab08caf0bed5ec
lmhufnagel <lmhufnagel@gmail.com>
2017-08-22 13:24:18 -0400

    new methods

--- app/models/cliinterface.rb
+++ app/models/cliinterface.rb
@@ -1,25 +1,30 @@
-def valid_account (username)
-  "Please enter your username"
-  name = gets.chomp
-  if find_member_by_username(name)
-    puts welcome
-  else
-    puts "Youre not yet a member! Lets change that! Please return to the homepage and create an account"
+class CommandLineInterface
+
+  def valid_account
+    puts "Please enter your username"
+    name = gets.chomp
+    binding.pry
+    if Member.find_by(username: name)
+      puts welcome
+    else
+      puts "Bummer, looks like you're not a member yet! Lets change that! Please return to the homepage and create an account"
+    end
+    binding.pry
   end
-end
 
-def welcome
-  puts "Hey there! #{username}! We want to hear what you have to say!"
-end
+  def welcome
+    puts "Hey there! #{username}! We want to hear what you have to say!"
+  end
 
-def which_project
-  puts"Which project are you interested in?"
-  gets.chomp
-end
+  def which_project
+    puts"Which project are you interested in?"
+    gets.chomp
+  end
 
-def get_character_from_user
-  puts "What do you think will make our project better? Please drop a note in our suggestion box!"
-  suggestion = gets.chomp
+  def get_suggestion_from_user
+    puts "What do you think will make our project better? Please drop a note in our suggestion box!"
+    gets.chomp
+  end
 end
 
 # def add_date_time
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
   has_many :suggestions
+  has_many :projects, through: :suggestions
 end
