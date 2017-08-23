db98207566c1106a1739e430e6262bbbf1793ff5
lmhufnagel <lmhufnagel@gmail.com>
2017-08-23 11:10:17 -0400

    need to merge

--- app/models/cliinterface.rb
+++ app/models/cliinterface.rb
@@ -1,6 +1,7 @@
 
 class CommandLineInterface
 
+
   def valid_account
     puts "Please enter your username"
     name = gets.chomp
@@ -13,33 +14,49 @@ class CommandLineInterface
       Member.create(username: name)
 
     end
-
-
-
   end
 
   def welcome(name)
     puts "Hey there #{name}! We want to hear what you have to say!"
   end
 
+  def which_category
+    puts "Select the number of the category that interests you."
+    list_categories
+    category = gets.chomp
+    # then based on input, outputs list of possible projects in that category
+  end
+
+
+  # category = gets.chomp
+
+  @@categories = {1 => "Local Businesses", 2 => "Community Projects", 3 => "Culture", 4 => "Health & Fitness", 5 => "Tech & Innovation", 6 => "Home"}
+
+   def list_categories
+     @@categories.each_with_index do |(key, value), index|
+    puts "#{index+1}: #{value}"
+   end
+ end
+
+
+  #  def list_projects
+
+
   def which_project
     puts "Which project are you interested in?"
     project = gets.chomp
     Project.find_by(title: project)
+    # if Project.find_by(title: project)
+    #   get_suggestion_from_user
+    # else
   end
 
   def get_suggestion_from_user(name, project)
     puts "What do you think will make our project better? Please drop a note in our suggestion box!"
     sugg = gets.chomp
-    
+
     Suggestion.find_or_create_by(suggestion: sugg, memeber_id: name.id, project_id: project.id)
 
 
   end
 end
-
-# def add_date_time
-#   change_table 'MYTABLE' do |table|
-#     add_column(:mytable, :my_field_name, :integer)
-#   end
-# end
